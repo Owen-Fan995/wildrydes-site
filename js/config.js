5c245b935b34b35afc16e0979c07cf3703a41925
Owen-Fan995 <131774408+Owen-Fan995@users.noreply.github.com>
2025-11-08 11:12:09 +1100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,10 +2,11 @@ window._config = {
     cognito: {
         userPoolId: 'ap-sou7ldgo35h71q67eknmnm7r9cr2e', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'theast-2_SjWxh6ERg' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
